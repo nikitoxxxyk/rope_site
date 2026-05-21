907f2f2d2184ce902fb957937e96d6194d191706
Никита <kuprys.nik@mail.ru>
2026-05-21 21:44:02 +0300

    Change mail name

--- catalog/views.py
+++ catalog/views.py
@@ -139,8 +139,8 @@ def send_order_email(order):
 	send_mail(
         subject=f'Новый заказ #{order.id}',
         message=plain_message,
-        from_email='nicitakuprys@yandex.ru',
-        recipient_list=['nicitakuprys@yandex.ru'],
+        from_email='lentamarket92@gmail.com',
+        recipient_list=['lentamarket92@gmail.com'],
 		# recipient_list=['rodstvennik@yandex.ru', order.email] - дублировать клиенту
 		html_message=html_message,
 		fail_silently=False,
@@ -195,8 +195,8 @@ def contact_view(request):
 			send_mail(
 				subject=subject,
 				message=body,
-				from_email='nicitakuprys@yandex.ru',
-				recipient_list=['nicitakuprys@yandex.ru'],
+				from_email='lentamarket92@gmail.com',
+				recipient_list=['lentamarket92@gmail.com'],
 				fail_silently=False,
 				)
 			return render(request, 'contact_success.html')
@@ -229,8 +229,8 @@ def call_request(request):
 			send_mail(
 				subject=subject,
 				message=body,
-				from_email='nicitakuprys@yandex.ru',
-				recipient_list=['nicitakuprys@yandex.ru'],
+				from_email='lentamarket92@gmail.com',
+				recipient_list=['lentamarket92@gmail.com'],
 				fail_silently=False,
 			)
 			return render(request, 'call_success.html')
