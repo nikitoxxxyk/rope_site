ebe04d45304b62d33cd5dc888dbddb628a9f1552
Никита <kuprys.nik@mail.ru>
2026-07-29 19:17:13 +0300

    Docker-compose changed

--- rope_site/settings.py
+++ rope_site/settings.py
@@ -171,11 +171,14 @@ UNFOLD = {
 }
 
 # Для почты
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.hoster.by'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+if 'runserver' in sys.argv or 'docker' in sys.argv:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBacken' # Если проект запущен локально
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.hoster.by'
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
